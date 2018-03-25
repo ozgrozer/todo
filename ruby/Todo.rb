b34f060c08ac523592d4863b98ac60051fab3018
Ozgur <ozgr@live.com>
2018-03-25 12:42:54 +0300

    Continue Ruby.

--- ruby/Todo.rb
+++ ruby/Todo.rb
@@ -1,5 +1,55 @@
+require 'json'
+
 class Todo
-  def writeFile
+  def initialize
+    @dbFilePath = './../db.json'
+    @dbFileContent = JSON.parse(readFile())
+  end
+
+  def writeFile (fileContent = '')
     return 'writeFile'
+  end
+
+  def readFile
+    if File.exist?(@dbFilePath)
+      return File.read(@dbFilePath)
+    else
+      writeFile('{}')
+      return '{}'
+    end
+  end
+
+  def addTask (taskTitle)
+    biggestKey = @dbFileContent.map { |quote| quote[0].to_i }.max
+    newTaskId = biggestKey ? (biggestKey + 1) : 1
+    @dbFileContent[newTaskId] = { title: taskTitle, done: 0 }
+    return writeFile() ? 'added: ' + taskTitle : 'error'
+  end
+
+  def editTask (taskId, taskTitle)
+    return 'editTask'
+  end
+
+  def markAsDone (taskId)
+    return 'markAsDone'
+  end
+
+  def markAsUndone (taskId)
+    return 'markAsUndone'
+  end
+
+  def removeTask (taskId)
+    return 'removeTask'
+  end
+
+  def listTasks
+    tasks = "ID\tDone\tTask\n===================="
+    @dbFileContent.each do |key|
+      @id = key[0]
+      @title = key[1]['title']
+      @done = key[1]['done'] === 1 ? '[x]' : '[ ]'
+      tasks += "\n##@id\t#@done\t#@title"
+    end
+    return tasks
   end
 end
--- ruby/app.rb
+++ ruby/app.rb
@@ -2,4 +2,47 @@ require_relative 'Todo'
 
 todo = Todo.new()
 
-puts todo.writeFile()
+argv1 = ARGV[0]
+argv2 = ARGV[1]
+argv3 = ARGV[2]
+
+result = ''
+if argv1 === '-a' or argv1 === '--add'
+  result = todo.addTask(argv2)
+elsif argv1 === '-l' or argv1 === '--list'
+  result = todo.listTasks()
+elsif argv1 === '-e' or argv1 === '--edit'
+  result = todo.editTask(argv2, argv3)
+elsif argv1 === '-d' or argv1 === '--done'
+  result = todo.markAsDone(argv2)
+elsif argv1 === '-u' or argv1 === '--undone'
+  result = todo.markAsUndone(argv2)
+elsif argv1 === '-r' or argv1 === '--remove'
+  result = todo.removeTask(argv2)
+else
+  result = '
+Available commands:
+
+Add new task:
+$ ruby app.rb -a|--add "<taskTitle>"
+
+List all tasks:
+$ ruby app.rb -l|--list
+
+Edit task:
+$ ruby app.rb -e|--edit <taskId> "<taskTitle>"
+
+Mark as done:
+$ ruby app.rb -d|--done <taskId>
+
+Mark as undone:
+$ ruby app.rb -u|--undone <taskId>
+
+Remove task:
+$ ruby app.rb -r|--remove <taskId>
+
+This screen:
+$ ruby app.rb'
+end
+
+puts result
